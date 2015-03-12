c548c824786efcca1c90c041495916b615a03de1
Ivan Garcia <alu0100693737@ull.edu.es>
2015-03-12 20:19:46 +0000

    Añadiendo travis y karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,79 +1,54 @@
 // Karma configuration
 
-
 module.exports = function(config) {
-  config.set({
+config.set({
+// base path that will be used to resolve all patterns (eg. files, exclude)
+basePath: '',
+// frameworks to use
+// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+frameworks: ['mocha', 'chai'],
+client: {
+  mocha: {
+    ui: 'tdd'
+  }
+},
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
-
-
-    // frameworks to use
-    frameworks: ['mocha'],
-
-    client: {
-         mocha: {
-           ui: 'tdd'
-        }   
-     },
-     
-     preprocessors: {
-          'tests/index.html': ['html2js']
-    },
-     
-    // list of files / patterns to load in the browser
-    files: [
-            'tests/index.html',
-            'tests/*.js',
-            'csv.js'
-    ],
-
-
-    // list of files to exclude
-    exclude: [
-      
-    ],
-
-    
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera (has to be installed with `npm install karma-opera-launcher`)
-    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-    // - PhantomJS
-    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: ['firefox'],
-
-
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
-
-
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-  });
-};
+// list of files / patterns to load in the browser
+files: [
+  'htpp://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.min.js',
+  'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js',
+  'tests/karmatest.html',
+  '*.js',
+  'tests/*.js'
+],
+// list of files to exclude
+exclude: [
+  'gulpfile.js',
+  'tests/test.js'
+],
+// preprocess matching files before serving them to the browser
+// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+preprocessors: {
+  'tests/karmatest.html': ['html2js']
+},
+// test results reporter to use
+// possible values: 'dots', 'progress'
+// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+reporters: ['progress'],
+// web server port
+port: 9876,
+// enable / disable colors in the output (reporters and logs)
+colors: true,
+// level of logging
+// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+logLevel: config.LOG_INFO,
+// enable / disable watching file and executing tests whenever any file changes
+autoWatch: true,
+// start these browsers
+// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+browsers: [ 'PhantomJS', 'Firefox', 'Chrome'],
+// Continuous Integration mode
+// if true, Karma captures browsers, runs the tests and exits
+singleRun: false
+});
