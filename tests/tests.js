3ce2dca63399012d743ed5258cb0a7a90854e5bb
Ivan Garcia <alu0100693737@ull.edu.es>
2015-03-19 06:37:17 +0000

    Añadiendo ajax

--- tests/tests.js
+++ tests/tests.js
@@ -10,33 +10,33 @@ finaltable = document.getElementById('finaltable');
 
 test('Intr. correcta', function () {
 original.value = 'a, b, c, d\naa, bb, cc, dd';
-calculate();
+calculateN();
 var esperado =  '\n\t    <table id="result" class="center">\n\t    \n\t    <tbody><tr class="">\n\t    \n\t    <td>a</td>\n\t    \n\t    <td> b</td>\n\t    \n\t    <td> c</td>\n\t    \n\t    <td> d</td>\n\t    \n\t    </tr>\n\t    \n\t    <tr class="">\n\t    \n\t    <td>aa</td>\n\t    \n\t    <td> bb</td>\n\t    \n\t    <td> cc</td>\n\t    \n\t    <td> dd</td>\n\t    \n\t    </tr>\n\t    \n\t    </tbody></table>\n\t'
 assert.deepEqual(finaltable.innerHTML, esperado);
 });
 test('Almacenamiento local', function () {
 original.value='Funciona, el, almacenamiento, interno';
-calculate();
+calculateN();
 assert.deepEqual(localStorage.original, 'Funciona, el, almacenamiento, interno');
 });
 
 
 test('Tabla normal', function () {
 original.value = '1,2,3\n4,5,6';
-calculate();
+calculateN();
 var valueExpected = '\n\t    <table id="result" class="center">\n\t    \n\t    <tbody><tr class="">\n\t    \n\t    <td>1</td>\n\t    \n\t    <td>2</td>\n\t    \n\t    <td>3</td>\n\t    \n\t    </tr>\n\t    \n\t    <tr class="">\n\t    \n\t    <td>4</td>\n\t    \n\t    <td>5</td>\n\t    \n\t    <td>6</td>\n\t    \n\t    </tr>\n\t    \n\t    </tbody></table>\n\t';
 assert.deepEqual(finaltable.innerHTML, valueExpected);
 });
 test('Tabla compleja', function () {
 original.value = '1,2,3,"4,5"\n6,7,8,"9,10"';
-calculate();
+calculateN();
 var valueExpected = '\n\t    <table id="result" class="center">\n\t    \n\t    <tbody><tr class="">\n\t    \n\t    <td>1</td>\n\t    \n\t    <td>2</td>\n\t    \n\t    <td>3</td>\n\t    \n\t    <td>4,5</td>\n\t    \n\t    </tr>\n\t    \n\t    <tr class="">\n\t    \n\t    <td>6</td>\n\t    \n\t    <td>7</td>\n\t    \n\t    <td>8</td>\n\t    \n\t    <td>9,10</td>\n\t    \n\t    </tr>\n\t    \n\t    </tbody></table>\n\t';
 assert.deepEqual(finaltable.innerHTML, valueExpected);
 });
 
 test('Creacion de tabla erronea', function () {
 original.value = '1,2,3,"4,5"\n6,7,8,"9,7",b';
-calculate();
+calculateN();
 var valueExpected = '\n\t    <table id="result" class="center">\n\t    \n\t    <tbody><tr class="">\n\t    \n\t    <td>1</td>\n\t    \n\t    <td>2</td>\n\t    \n\t    <td>3</td>\n\t    \n\t    <td>4,5</td>\n\t    \n\t    </tr>\n\t    \n\t    <tr class="error">\n\t    \n\t    <td>6</td>\n\t    \n\t    <td>7</td>\n\t    \n\t    <td>8</td>\n\t    \n\t    <td>9,7</td>\n\t    \n\t    <td>b</td>\n\t    \n\t    </tr>\n\t    \n\t    </tbody></table>\n\t';
 assert.deepEqual(finaltable.innerHTML, valueExpected);
 });
