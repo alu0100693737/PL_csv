6cb2fb3d002618fd7b4005a946e5c069d67a7a7f
Ivan Garcia <alu0100693737@ull.edu.es>
2015-03-19 03:52:31 +0000

    Arreglando problemas travis, karma

--- karma.conf.js
+++ karma.conf.js
@@ -47,9 +47,9 @@ module.exports = function(config) {
       autoWatch: true,
       // start these browsers
       // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-      browsers: [ 'Chrome', 'PhanthomJS'],
+      browsers: ['PhanthomJS'],
       // Continuous Integration mode
       // if true, Karma captures browsers, runs the tests and exits
-      singleRun: true
+      singleRun: false
   });
 };
