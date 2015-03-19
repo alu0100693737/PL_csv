5ba534a89e3b0ae09781465101ad70bd6be1688f
Ivan Garcia <alu0100693737@ull.edu.es>
2015-03-19 03:35:30 +0000

    Nuevos Cambios

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
+var _ = require('underscore');
+var $ = require('jquery');
 var express = require('express');
 var app = express();
 var path = require('path');
 var expressLayouts = require('express-ejs-layouts');
-app.set('port', (process.env.PORT || 3000));
-// view engine setup
+app.set('port', (process.env.PORT || 5000));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//app.set('layout', 'myLayout') // defaults to 'layout' '
 app.use(expressLayouts);
-//app.use(express.static('public'));
-app.use(express.static(__dirname + '/public'));
-app.get('/', function (req, res) {
-res.render('index', { title: 'Express' });
-})
-
-app.get('/csv-pl', function (req, res) {
-var isAjaxRequest = req.xhr;
-console.log(isAjaxRequest);
-if (isAjaxRequest) {
-console.log(req.query);
-res.send('{"answer": "Server responds: hello world!"}')
-}
-else {
-res.send('not an ajax request');
-}
+app.use(express.static(__dirname + '/'));
+app.get('/', function (request, response) {
+response.render('index', { title: 'Comma Separated Value Analyzer' });
 });
-var server = app.listen(app.get('port'), function () {
-var host = server.address().address
-var port = server.address().port
-console.log('Example app listening at http://%s:%s', host, port)
+app.listen(app.get('port'), function () {
+console.log("Node app is running at localhost:" + app.get('port'));
 });
