18293ed38b67e48b40d66de5ba7c00d5d71df649
Ivan Garcia <alu0100693737@ull.edu.es>
2015-03-19 06:27:30 +0000

    Añadiendo ajax

--- csv.js
+++ csv.js
@@ -1,55 +1,42 @@
-
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
-$(document).ready(function () {
-
-  $("button").click(function () {
-  calculate();
-  });
+$(document).ready(function() {
+// Cambiar los símbolos <% %> de underScore por los simbolos {{ }}
+// para que no creen conflictos con express.js
+_.templateSettings = {
+evaluate : /\{\{([\s\S]+?)\}\}/g,
+interpolate : /\{\{=([\s\S]+?)\}\}/g,
+escape : /\{\{-([\s\S]+?)\}\}/g
+};
+$("button").click(function() {
+calculateN ();
 });
-
-function calculate() {
-var result;
+});
+function calculateN () {
 var original = document.getElementById("original");
 var temp = original.value;
-var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
-var lines = temp.split(/\n+\s*/);
-var commonLength = NaN;
-var r = [];
+var linesI = temp.split(/\n+\s*/);
+var lines = JSON.stringify(linesI);
 if (window.localStorage) localStorage.original = temp;
-for (var t in lines) {
-var temp = lines[t];
-var m = temp.match(regexp);
-var result = [];
-var error = false;
-if (m) {
-if (commonLength && (commonLength != m.length)) {
-error = true;
+$.ajax({
+url: '/ajaxEx/'+ lines +'/',
+type: 'GET',
+async: false,
+cache: false,
+timeout: 30000,
+dataType: 'JSON',
+success: function (data) {
+$('body').append(data);
+render(data);
 }
-else {
-commonLength = m.length;
-error = false;
+});
 }
-for (var i in m) {
-var removecomma = m[i].replace(/,\s*$/, '');
-var remove1stquote = removecomma.replace(/^\s*"/, '');
-var removelastquote = remove1stquote.replace(/"\s*$/, '');
-var removeescapedquotes = removelastquote.replace(/\\"/, '"');
-result.push(removeescapedquotes);
-}
-var rowclass = error? 'error' : '';
-r.push({ value: result, rowClass: rowclass });
-}
-else {
-alert('La fila "' + temp + '" no es un valor de CSV permitido.');
-error = true;
-}
-}
-var template = fillTable.innerHTML;
-finaltable.innerHTML = _.template(template, {items: r});
-}
-window.onload = function () {
+window.onload = function() {
 // If the browser supports localStorage and we have some stored data
 if (window.localStorage && localStorage.original) {
 document.getElementById("original").value = localStorage.original;
 }
 };
+function render(rows){
+var resultTemplate = document.getElementById("resultTemplate").innerHTML;
+$('#finaltable').html( _.template(resultTemplate, { rows: rows }));
+}
