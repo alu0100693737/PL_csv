b321eb306c8136a7397d38c3af515c46b66a07ee
Ivan Garcia <alu0100693737@ull.edu.es>
2015-03-19 02:28:56 +0000

    Arreglado karma

--- tests/karma.js
+++ tests/karma.js
@@ -10,7 +10,7 @@ suite('csv', function() {
     test('Intr. correcta', function () {
 	original.value = 'a, b, c, d\naa, bb, cc, dd';
 	calculate();
-	var esperado = '\n\t <table id="result" class="center">\n\t \n\t <tbody><tr class="">\n\t \n\t <td>a</td>\n\t \n\t <td> b</td>\n\t \n\t <td> c</td>\n\t \n\t <td> d</td>\n\t \n\t </tr>\n\t \n\t <tr class="">\n\t \n\t <td>aa</td>\n\t \n\t <td> bb</td>\n\t \n\t <td> cc</td>\n\t \n\t <td> dd</td>\n\t \n\t </tr>\n\t \n\t </tbody></table>\n\t'
+	var esperado = '\n\t    <table id="result" class="center">\n\t    \n\t    <tbody><tr class="">\n\t    \n\t    <td>a</td>\n\t    \n\t    <td> b</td>\n\t    \n\t    <td> c</td>\n\t    \n\t    <td> d</td>\n\t    \n\t    </tr>\n\t    \n\t    <tr class="">\n\t    \n\t    <td>aa</td>\n\t    \n\t    <td> bb</td>\n\t    \n\t    <td> cc</td>\n\t    \n\t    <td> dd</td>\n\t    \n\t    </tr>\n\t    \n\t    </tbody></table>\n\t'
 	assert.deepEqual(finaltable.innerHTML, esperado);
     });
     test('Almacenamiento local', function () {
@@ -21,19 +21,19 @@ suite('csv', function() {
     test('Tabla normal', function () {
 	original.value = '1,2,3\n4,5,6';
 	calculate();
-	var valueExpected = '\n\t <table id="result" class="center">\n\t \n\t <tbody><tr class="">\n\t \n\t <td>1</td>\n\t \n\t <td>2</td>\n\t \n\t <td>3</td>\n\t \n\t </tr>\n\t \n\t <tr class="">\n\t \n\t <td>4</td>\n\t \n\t <td>5</td>\n\t \n\t <td>6</td>\n\t \n\t </tr>\n\t \n\t </tbody></table>\n\t';
+	var valueExpected = '\n\t    <table id="result" class="center">\n\t    \n\t    <tbody><tr class="">\n\t    \n\t    <td>1</td>\n\t    \n\t    <td>2</td>\n\t    \n\t    <td>3</td>\n\t    \n\t    </tr>\n\t    \n\t    <tr class="">\n\t    \n\t    <td>4</td>\n\t    \n\t    <td>5</td>\n\t    \n\t    <td>6</td>\n\t    \n\t    </tr>\n\t    \n\t    </tbody></table>\n\t';
 	assert.deepEqual(finaltable.innerHTML, valueExpected);
     });
     test('Tabla compleja', function () {
 	original.value = '1,2,3,"4,5"\n6,7,8,"9,10"';
 	calculate();
-	var valueExpected = '\n\t <table id="result" class="center">\n\t \n\t <tbody><tr class="">\n\t \n\t <td>1</td>\n\t \n\t <td>2</td>\n\t \n\t <td>3</td>\n\t \n\t <td>4,5</td>\n\t \n\t </tr>\n\t \n\t <tr class="">\n\t \n\t <td>6</td>\n\t \n\t <td>7</td>\n\t \n\t <td>8</td>\n\t \n\t <td>9,10</td>\n\t \n\t </tr>\n\t \n\t </tbody></table>\n\t';
+	var valueExpected = '\n\t    <table id="result" class="center">\n\t    \n\t    <tbody><tr class="">\n\t    \n\t    <td>1</td>\n\t    \n\t    <td>2</td>\n\t    \n\t    <td>3</td>\n\t    \n\t    <td>4,5</td>\n\t    \n\t    </tr>\n\t    \n\t    <tr class="">\n\t    \n\t    <td>6</td>\n\t    \n\t    <td>7</td>\n\t    \n\t    <td>8</td>\n\t    \n\t    <td>9,10</td>\n\t    \n\t    </tr>\n\t    \n\t    </tbody></table>\n\t';
 	assert.deepEqual(finaltable.innerHTML, valueExpected);
     });
     test('Creacion de tabla erronea', function () {
 	original.value = '1,2,3,"4,5"\n6,7,8,"9,7",b';
 	calculate();
-	var valueExpected = '\n\t <table id="result" class="center">\n\t \n\t <tbody><tr class="">\n\t \n\t <td>1</td>\n\t \n\t <td>2</td>\n\t \n\t <td>3</td>\n\t \n\t <td>4,5</td>\n\t \n\t </tr>\n\t \n\t <tr class="error">\n\t \n\t <td>6</td>\n\t \n\t <td>7</td>\n\t \n\t <td>8</td>\n\t \n\t <td>9,7</td>\n\t \n\t <td>b</td>\n\t \n\t </tr>\n\t \n\t </tbody></table>\n\t';
+	var valueExpected = '\n\t    <table id="result" class="center">\n\t    \n\t    <tbody><tr class="">\n\t    \n\t    <td>1</td>\n\t    \n\t    <td>2</td>\n\t    \n\t    <td>3</td>\n\t    \n\t    <td>4,5</td>\n\t    \n\t    </tr>\n\t    \n\t    <tr class="error">\n\t    \n\t    <td>6</td>\n\t    \n\t    <td>7</td>\n\t    \n\t    <td>8</td>\n\t    \n\t    <td>9,7</td>\n\t    \n\t    <td>b</td>\n\t    \n\t    </tr>\n\t    \n\t    </tbody></table>\n\t';
 	assert.deepEqual(finaltable.innerHTML, valueExpected);
      });
 });
