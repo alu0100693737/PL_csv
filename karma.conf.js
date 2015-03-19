fa67cc4dfe821e574b4d7911f390a907bcffaece
Ivan Garcia <alu0100693737@ull.edu.es>
2015-03-19 00:56:56 +0000

    Mas modificaciones con karma.html

--- karma.conf.js
+++ karma.conf.js
@@ -25,6 +25,7 @@ module.exports = function(config) {
       // list of files to exclude
       exclude: [
 	'gulpfile.js',
+	'tests/test.js'
       ],
       // preprocess matching files before serving them to the browser
       // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
